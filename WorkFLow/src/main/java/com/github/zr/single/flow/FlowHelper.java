923b30c9ade1bf8e1a8997aba331fcaa7864a784
RuiZhong1 <Rui_Zhong1@human-horizons.com>
2023-02-28 09:47:40 +0800

    逻辑优化

--- WorkFLow/src/main/java/com/github/zr/single/flow/FlowHelper.java
+++ WorkFLow/src/main/java/com/github/zr/single/flow/FlowHelper.java
@@ -9,44 +9,33 @@ import com.github.zr.single.flow.listener.FlowErrorObserver;
 import com.github.zr.single.flow.listener.FlowFunction;
 import com.github.zr.single.flow.listener.FlowNextObserver;
 
-class FlowHelper<T, R> extends BaseObservable {
+public class FlowHelper<T, R> extends BaseObservable {
     private FlowFunction function;
     private FlowNextObserver<R> observerNext = new FlowNextObserver<R>() {
         @Override
         public void next(final R obj) throws Exception {
             if (subscriber != null) {
-                if (done) {
-                    return;
-                }
                 subscriber.onNext(obj);
             }
         }
     };
     private FlowCompleteObserver observerComplete = new FlowCompleteObserver() {
         @Override
-        public void onComplete(final Object obj)   {
+        public void onComplete(final Object obj) {
             if (subscriber != null) {
-                if (done) {
-                    return;
-                }
-                done = true;
                 subscriber.onComplete(obj);
             }
         }
     };
     private FlowErrorObserver observerError = new FlowErrorObserver() {
         @Override
-        public void onError(final Throwable code,final Object obj)  {
-
+        public void onError(final Throwable code, final Object obj) {
             if (subscriber != null) {
-                if (done) {
-                    return;
-                }
-                done = true;
                 subscriber.onError(code, obj);
             }
         }
     };
+
 
     public FlowHelper(FlowFunction<? super T, ? extends R> function) {
         this.function = function;
@@ -56,36 +45,64 @@ class FlowHelper<T, R> extends BaseObservable {
         this.setWorkScheduler(workScheduler);
         this.function = function;
     }
-    private volatile boolean done;
-    private Observer subscriber;
+
+    private  Observer subscriber;
+
     public <R> Observer call(final Observer<? super R> sub) {
-        done=false;
-        this.subscriber=sub;
+        this.subscriber = sub;
         return new Observer<T>() {
+            boolean done = false;
             @Override
             public void onNext(final T obj) {
+                if (done) {
+                    return;
+                }
                 if (function != null) {
-                    try {
-                        function.next(obj,observerNext);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        onError(e, e.getMessage());
-                        return;
-                    }
+                    execute(getScheduler(), new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                function.next(obj, observerNext);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                                onError(e, e.getMessage());
+                                return;
+                            }
+                        }
+                    });
                 }
             }
 
             @Override
             public void onComplete(final Object obj) {
-                if (function != null) {
-                    function.complete(obj, observerComplete);
+                if (done) {
+                    return;
                 }
+                done = true;
+                execute(getScheduler(), new Runnable() {
+                    @Override
+                    public void run() {
+                        if (function != null) {
+                            function.complete(obj, observerComplete);
+                        }
+                    }
+                });
             }
+
             @Override
             public void onError(final Throwable code, final Object obj) {
-                if (function != null) {
-                    function.error(code, obj,observerError);
+                if (done) {
+                    return;
                 }
+                done = true;
+                execute(getScheduler(), new Runnable() {
+                    @Override
+                    public void run() {
+                        if (function != null) {
+                            function.error(code, obj, observerError);
+                        }
+                    }
+                });
             }
         };
     }
